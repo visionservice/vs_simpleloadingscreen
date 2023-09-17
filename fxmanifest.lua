bf366715e01cf6a411c0bf169130c020d49cd226
Nikquz <91543569+Nikquz@users.noreply.github.com>
2023-09-17 12:08:04 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+author '#Nikquz#9748'
+description 'VS SIMPLE LOADING SCREEN'
+
+loadscreen("web/index.html")
+loadscreen_cursor 'yes'
+
+files {
+	"web/*.*",
+    "web/images/*.*"
+}
